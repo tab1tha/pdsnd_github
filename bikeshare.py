943a0bdb744d6e2e0d5b1aeef66f9eedf0da0f4d
Tambe Tabitha Achere <tambetachere@gmail.com>
2020-01-24 18:23:34 +0100

    This provides a more detailed description of user_stats()

--- bikeshare.py
+++ bikeshare.py
@@ -140,7 +140,9 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics on bikeshare users including the user types and their respective 
+    frequencies. Also, if in cases where the gender information of the bikers is provided, the gender 
+    distribution and age distribution is highlighted"""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
